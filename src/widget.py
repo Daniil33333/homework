b5bc559f787d2b24a5766c86cdb6474a7ca81830
Daniil <zaitsevdo33@gmail.com>
2025-04-13 10:52:35 +0300

    2025.04.13 version2

--- src/widget.py
+++ src/widget.py
@@ -6,3 +6,8 @@ def mask_account_card(str_1:str) -> str:
     else:
         return str_1[:-16] + masks.get_mask_card_number(str_1[-16:])
 
+
+def get_date (str_1:str) -> str:
+    return str_1[8:10] + "." + str_1[5:7] + "." + str_1[:4]
+
+print(get_date("2024-03-11T02:26:18.671407"))
