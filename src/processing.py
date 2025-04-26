2ad181e1ff22e85a06b1fc5aa61542939a047939
Daniil <zaitsevdo33@gmail.com>
2025-04-26 15:27:41 +0300

    2025.04.26 hw del comments processing.py

--- src/processing.py
+++ src/processing.py
@@ -12,14 +12,6 @@ def filter_by_state(dict_list: list[dict], state:str = 'EXECUTED') -> list[dict]
     return result_list
 
 
-# print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-#                       state='CANCELED'))
-
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-
 def sort_by_date(date_list: list[dict], order=True) -> list[dict]:
     """
     Функция возвращает новый список, отсортированный по дате (date).
@@ -27,13 +19,3 @@ def sort_by_date(date_list: list[dict], order=True) -> list[dict]:
     result_list = sorted(date_list, key=lambda x: x['date'], reverse=order)
     return result_list
 
-# print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-# ))
-#
-# print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}], order=False
-# ))
-#
-# a = {'name': 'Sam', 'age': 19}
-#
-# f1 = lambda x: x['age']
-# print (f1(a))
